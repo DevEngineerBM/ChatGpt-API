19167426090773c599e5337981eef937f07cb231
belkhalil <belkhalil1mohamed@gmail.com>
2024-03-07 22:55:07 +0100

    PROJECT DONE

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//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
+document.getElementById("submit").addEventListener("click", sendToChatGPT);
+
+async function sendToChatGPT() {
+  let value = document.getElementById('input').value;
+
+  let body = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: value }],
+    temperature: 0.7,
+  };
+
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ****************************'
+  };
+
+  try {
+    let response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      let data = await response.json();
+      let reply = data.choices[0].message.content;
+      document.getElementById('replay').textContent = reply;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
